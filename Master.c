1c3693e7ea5be14ffa7178daac34ac8392208d4f
NickLockett <NickS.Lockett@gmail.com>
2017-10-10 16:04:35 -0400

    Fixed directory paointer bug

--- Master.c
+++ Master.c
@@ -7,6 +7,9 @@
 #include <dirent.h>
 #include <ftw.h>
 
+int s_builder(const char *, const struct stat64 *, int, struct FTW *);
+int image();
+
 const int MAX_METADATA = 2000;
 
 metadata* meta;
@@ -31,11 +34,10 @@ int main(int argc, char **argv){
     int result =  nftw64(argv[1], 	// filepath
     				s_builder,      // function to call on each directory/file
     				MAX_METADATA, 	// max number of directories that can be used
-    				0);				// flags to specialize usage, we aren't using any right now  
-
+    				FTW_DEPTH);		// flags to specialize usage, we aren't using any right now  
 
     // Now write the file to a structure
-
+    image();
 	return 0;
 }
 
@@ -53,6 +55,8 @@ int s_builder(const char * path_name, const struct stat64 * object_info, int ftw
 		// get number of files in directory algo from: https://stackoverflow.com/questions/1723002/how-to-list-all-subdirectories-in-a-given-directory-in-c?answertab=votes#tab-top
 		int dir_length = 0;
     	struct dirent* d;
+    	// TODO Consider this- 2 loops or one loop and then rebuilding loop?
+    	metadata* subFilesOver = (metadata*) malloc(100000*sizeof(metadata));
     	DIR* rdir = opendir(path_name);
     	while((d = readdir(rdir)) != NULL)
     	{
@@ -61,19 +65,45 @@ int s_builder(const char * path_name, const struct stat64 * object_info, int ftw
             	perror(d->d_name);
         	}
         	else{
-        		dir_length++;
+        		if(strcmp(d->d_name, ".") == 0){
+        			// TODO: Check this
+        			subFiles[dir_length] = &metadata[metadataPointer];
+        		}
+        		if(strcmp(d->d_name, "..") == 0){
+        			// TODO: This is wrong but fixing it is mildly complex
+        			subFiles[dir_length] = &metadata[metadataPointer];
+        		}
+        		else{
+        			for(int j = metadataPointer-1; j >= 0; j--){
+        				// TODO: Check This
+        				if(strcmp(d->d_name, meta[j]->name)){
+        					subFiles[dir_length] = &metadata[j];
+        				}
+        			}
+        		}
+        		dir_length++;        		
         	}
     	}
     	closedir(rdir);
+
+    	//Build array to point to subfiles metadata
+    	// Currently doesn't deal with ..
+    	metadata* subFiles = (metadata*) malloc(dir_length*sizeof(metadata));
+    	for(int i = 0; i < dir_length; i++){
+    		subFiles[i] = subFilesOver[i];
+    	}
+
+    	for(int i = 0; i )
 		
 		// assign all values
 		meta[metadataPointer]->name = file_name;
 		meta[metadataPointer]->type = 1;
 		meta[metadataPointer]->length = dir_length;
 		meta[metadataPointer]->time = object_info->st_mtime;
-		meta[metadataPointer]->p = opendir(path_name);
+		meta[metadataPointer]->p = subFiles;
 		metadataPointer++;
 	}
+
 	// Store File Metadata - possibly add in FTW_NS and FTW_SNL functionality for failed symbolic links
 	else if((ftw == FTW_F) || (ftw == FTW_SL)){
 		//get filename
@@ -95,3 +125,20 @@ int s_builder(const char * path_name, const struct stat64 * object_info, int ftw
 }
 
 // function to write the file 
+int image(){
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
