25de33f189019aa0208a3dd1288f881f7bb43104
NickLockett <NickS.Lockett@gmail.com>
2017-10-15 16:55:57 -0400

    it compiles

--- Master.c
+++ Master.c
@@ -1,40 +1,43 @@
-#include "OnDiskStructure.h"
 #include <stdio.h>
-#include <iostream.h>
 #include <string.h>
 #include <assert.h>
 #include <sys/stat.h>
 #include <dirent.h>
 #include <ftw.h>
+#include <iostream>
+#include <stdint.h>
 
-int s_builder(const char *, const struct stat64 *, int, struct FTW *);
+#include "OnDiskStructure.h"
+
+
+int s_builder(const char *, const struct stat *, int, struct FTW *);
 int image();
 
 const int MAX_METADATA = 2000;
 
-metadata* meta;
+m_prs* meta;
 int metadataPointer = 0;
 
 int main(int argc, char **argv){
 	// initialize our structure
-	meta = (metadata*) malloc(MAX_METADATA * sizeof(metadata)); 
+	meta = (m_prs*) malloc(MAX_METADATA * sizeof(m_prs)); 
 	// ensure input is appropriate
     if(argc < 2){
-    	cout << "Please input a directory to master." << "\n";
+    	std::cout << "Please input a directory to master." << "\n";
     	return 0;
     }
     else if(argc > 2){
-    	cout << "This program currently only supports the mastering of a single directory.  
-    		Please package the directories into an additional folder and pass that in.  Thanks." << "\n";
+    	std::cout << "This program currently only supports the mastering of a single directory.  Please package the directories into an additional folder and pass that in.  Thanks." << "\n";
     	return 0;
     }
 
     // Using the nftw() funtion to update global structure
     //	see here: https://www.ibm.com/support/knowledgecenter/en/SSLTBW_2.3.0/com.ibm.zos.v2r3.bpxbd00/rnftw.htm
-    int result =  nftw64(argv[1], 	// filepath
-    				s_builder,      // function to call on each directory/file
-    				MAX_METADATA, 	// max number of directories that can be used
-    				0);		// flags to specialize usage, we aren't using any right now  
+            // filepath
+            // function to call on each directory/file
+            // max number of directories that can be used
+            // flags to specialize usage, we aren't using any right now  
+    int result =  nftw(argv[1], s_builder, MAX_METADATA, 0);		
 
     // Now write the file to a structure
     image();
@@ -42,21 +45,21 @@ int main(int argc, char **argv){
 }
 
 // function called on each sub directory/file, updates the global information
-int s_builder(const char * path_name, const struct stat64 * object_info, int ftw, struct FTW * data){
+int s_builder(const char * path_name, const struct stat * object_info, int ftw, struct FTW * data){
 
 	// store directory metadata
 	if(ftw == FTW_D){
 		//get filename
-		char* file_name = (char*) malloc( (sizeof(path_name)/sizeof(char) - FTW->base)*sizeof(char));
-		for(int i = 0 i < (sizeof(path_name)/sizeof(char) - FTW->base); i++){
-			file_name[i] = path_name[FTW->base + i];
+		char* file_name = (char*) malloc((sizeof(path_name)/sizeof(char) - data->base)*sizeof(char));
+		for(int i = 0; i < (sizeof(path_name)/sizeof(char) - data->base); i++){
+			file_name[i] = path_name[data->base + i];
 		}
 
-		// get number of files in directory algo from: https://stackoverflow.com/questions/1723002/how-to-list-all-subdirectories-in-a-given-directory-in-c?answertab=votes#tab-top
+		// get number of files in directory algo from: https://stackoverfloxw.com/questions/1723002/how-to-list-all-subdirectories-in-a-given-directory-in-c?answertab=votes#tab-top
 		int dir_length = 0;
     	struct dirent* d;
     	// TODO Consider this- 2 loops or one loop and then rebuilding loop?
-    	//metadata* subFilesOver = (metadata*) malloc(100000*sizeof(metadata));
+    	//metadata* subFilesOver = (metadata*) malloc(100000*sizeof(metadsata));
     	DIR* rdir = opendir(path_name);
     	while((d = readdir(rdir)) != NULL)
     	{
@@ -65,63 +68,39 @@ int s_builder(const char * path_name, const struct stat64 * object_info, int ftw
             	perror(d->d_name);
         	}
         	else{
-        		// Previous iteration saved the directories subfiles in a different format
-        		// if(strcmp(d->d_name, ".") == 0){
-        		// 	// TODO: Check this
-        		// 	subFiles[dir_length] = &metadata[metadataPointer];
-        		// }
-        		// if(strcmp(d->d_name, "..") == 0){
-        		// 	// TODO: This is wrong but fixing it is mildly complex
-        		// 	subFiles[dir_length] = &metadata[metadataPointer];
-        		// }
-        		// else{
-        		// 	for(int j = metadataPointer-1; j >= 0; j--){
-        		// 		// TODO: Check This
-        		// 		if(strcmp(d->d_name, meta[j]->name)){
-        		// 			subFiles[dir_length] = &metadata[j];
-        		// 		}
-        		// 	}
-        		// }
         		dir_length++;        		
         	}
     	}
     	closedir(rdir);
 
-    	//Build array to point to subfiles metadata
-    	// Currently doesn't deal with ..
-    	metadata* subFiles = (metadata*) malloc(dir_length*sizeof(metadata));
-    	for(int i = 0; i < dir_length; i++){
-    		subFiles[i] = subFilesOver[i];
-    	}
-
-    	for(int i = 0; i )
-		
 		// assign all values
-		meta[metadataPointer]->name = file_name;
-		meta[metadataPointer]->type = 1;
-		meta[metadataPointer]->length = dir_length;
-		meta[metadataPointer]->time = object_info->st_mtime;
-		meta[metadataPointer]->p = opendir(path_name);
+        for(int i =0; i < 255; i++){
+            meta[metadataPointer].name[i] = file_name[i];
+        }
+		meta[metadataPointer].type = DIRECTORY;
+		meta[metadataPointer].length = dir_length;
+		meta[metadataPointer].time = object_info->st_mtime;
+		meta[metadataPointer].p = opendir(path_name);
 		metadataPointer++;
 	}
 
 	// Store File Metadata - possibly add in FTW_NS and FTW_SNL functionality for failed symbolic links
 	else if((ftw == FTW_F) || (ftw == FTW_SL)){
 		//get filename
-		char* fileName = (char*) malloc( (sizeof(path_name)/sizeof(char) - FTW->base)*sizeof(char));
-		for(int i = 0 i < (sizeof(path_name)/sizeof(char) - FTW->base); i++){
-			file_name[i] = path_name[FTW->base + i];
+		char* file_name = (char*) malloc( (sizeof(path_name)/sizeof(char) - data->base)*sizeof(char));
+		for(int i = 0; i < (sizeof(path_name)/sizeof(char) - data->base); i++){
+			file_name[i] = path_name[data->base + i];
 		}
+        
 		// assign all values
-		meta[metadataPointer]->name = file_name;
-		meta[metadataPointer]->type = 0;
-		meta[metadataPointer]->length = object_info->st_size;
-		meta[metadataPointer]->time = object_info->st_mtime; // time of last modification, could also use atime for last access or ctime for last status change
-		meta[metadataPointer]->p = fopen(path_name, "r"); // open the file for reading, when writing to img use this stream
+        for(int i =0; i < 255; i++){
+            meta[metadataPointer].name[i] = file_name[i];
+        }		
+        meta[metadataPointer].type = PLAIN_FILE;
+		meta[metadataPointer].length = object_info->st_size;
+		meta[metadataPointer].time = object_info->st_mtime; // time of last modification, could also use atime for last access or ctime for last status change
+		meta[metadataPointer].p = fopen(path_name, "r"); // open the file for reading, when writing to img use this stream
 		metadataPointer++;
-	}
-	else{
-		//don't save anything for now, this could definitely cause problems
 	}
 }
 
--- OnDiskStructure.h
+++ OnDiskStructure.h
@@ -1,15 +1,35 @@
 #ifndef OnDiskStructure_H_   /* Include guard */
 #define OnDiskStructure_H_
 
-// structure for metadata on disk
-typedef struct metaData {
+
+
+// structure to store infomration while parsing
+struct metadata_parse {
 	char name[255];       // name of the file or directory
 	unsigned int type;    // indicates file or directory 0 - file 1 - directory
-	unsigned long length; // for file the length of the file in bytes, for directory the number of sub files/directories
-	unsigned long time;   // The time of access in UNIX time - saved as an unsigned long
+	uint64_t length; // for file the length of the file in bytes, for directory the number of sub files/directories
+	uint64_t time;   // The time of access in UNIX time - saved as an unsigned long
 	void* p;              // Pointer to either the start of the file or the subfiles/directories
 						   // if pointing to a file be sure to cast before using
-} metadata;
+};
+typedef struct metadata_parse m_prs;
+
+enum file_type {DIRECTORY = 0, PLAIN_FILE = 1, SYM_LINK = 2};
+
+
+// structure to store infomration while parsing
+struct metadata_header {
+	char name[255];       // name of the file or directory
+	enum file_type type;    // indicates file or directory 0 - file 1 - directory
+	uint64_t length;
+	uint64_t time;  
+	uint64_t offset;        
+					
+};
+typedef struct metadata_header m_hdr;
+
+
+
 
 
 
