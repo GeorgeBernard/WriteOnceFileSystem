bb3b90adf3ed8ee2d80ff3604fa15cf60a4ff082
NickLockett <NickS.Lockett@gmail.com>
2017-10-15 17:45:49 -0400

    fixed filename bug

--- Master.c
+++ Master.c
@@ -37,6 +37,7 @@ int main(int argc, char **argv){
             // function to call on each directory/file
             // max number of directories that can be used
             // flags to specialize usage, we aren't using any right now  
+
     int result =  nftw(argv[1], s_builder, MAX_METADATA, 0);		
 
     // Now write the file to a structure
@@ -46,20 +47,30 @@ int main(int argc, char **argv){
 
 // function called on each sub directory/file, updates the global information
 int s_builder(const char * path_name, const struct stat * object_info, int ftw, struct FTW * data){
-
+    std::cout << "Called s_builder on " << path_name << "\n";
 	// store directory metadata
 	if(ftw == FTW_D){
 		//get filename
-		char* file_name = (char*) malloc((sizeof(path_name)/sizeof(char) - data->base)*sizeof(char));
-		for(int i = 0; i < (sizeof(path_name)/sizeof(char) - data->base); i++){
-			file_name[i] = path_name[data->base + i];
-		}
+        char* path_name2;
+        strncpy(path_name2, path_name, strlen(path_name)); 
+        char* buf;
+        buf = std::strtok(path_name2, "/");
+        char* buff;
+        while (buf != NULL){   
+            buff = buf;
+            buf = std::strtok (NULL, "/");
+        }
+
+		char* file_name = (char*) malloc(sizeof(char)*256);
+        for(int i =0; i < sizeof(buff); i++){
+            file_name[i] = buff[i];
+        }
+        std::cout << file_name <<"\n";
 
 		// get number of files in directory algo from: https://stackoverfloxw.com/questions/1723002/how-to-list-all-subdirectories-in-a-given-directory-in-c?answertab=votes#tab-top
 		int dir_length = 0;
     	struct dirent* d;
     	// TODO Consider this- 2 loops or one loop and then rebuilding loop?
-    	//metadata* subFilesOver = (metadata*) malloc(100000*sizeof(metadsata));
     	DIR* rdir = opendir(path_name);
     	while((d = readdir(rdir)) != NULL)
     	{
@@ -72,28 +83,40 @@ int s_builder(const char * path_name, const struct stat * object_info, int ftw, 
         	}
     	}
     	closedir(rdir);
-
 		// assign all values
-        for(int i =0; i < 255; i++){
+        for(int i =0; i < 256; i++){
             meta[metadataPointer].name[i] = file_name[i];
         }
+
 		meta[metadataPointer].type = DIRECTORY;
 		meta[metadataPointer].length = dir_length;
 		meta[metadataPointer].time = object_info->st_mtime;
-		meta[metadataPointer].p = opendir(path_name);
-		metadataPointer++;
-	}
+		meta[metadataPointer].p = (void*) opendir(path_name);
 
+		metadataPointer++;
+        return 0;
+	}
 	// Store File Metadata - possibly add in FTW_NS and FTW_SNL functionality for failed symbolic links
 	else if((ftw == FTW_F) || (ftw == FTW_SL)){
 		//get filename
-		char* file_name = (char*) malloc( (sizeof(path_name)/sizeof(char) - data->base)*sizeof(char));
-		for(int i = 0; i < (sizeof(path_name)/sizeof(char) - data->base); i++){
-			file_name[i] = path_name[data->base + i];
-		}
-        
+        char* path_name2;
+        strncpy(path_name2, path_name, strlen(path_name)); 
+        char* buf;
+        buf = std::strtok(path_name2, "/");
+        char* buff;
+        while (buf != NULL){   
+            buff = buf;
+            buf = std::strtok (NULL, "/");
+        }
+
+        char* file_name = (char*) malloc(sizeof(char)*256);
+        for(int i =0; i < sizeof(buff); i++){
+            file_name[i] = buff[i];
+        }
+        std::cout << file_name <<"\n";
+
 		// assign all values
-        for(int i =0; i < 255; i++){
+        for(int i =0; i < 256; i++){
             meta[metadataPointer].name[i] = file_name[i];
         }		
         meta[metadataPointer].type = PLAIN_FILE;
@@ -101,6 +124,7 @@ int s_builder(const char * path_name, const struct stat * object_info, int ftw, 
 		meta[metadataPointer].time = object_info->st_mtime; // time of last modification, could also use atime for last access or ctime for last status change
 		meta[metadataPointer].p = fopen(path_name, "r"); // open the file for reading, when writing to img use this stream
 		metadataPointer++;
+        return 0;
 	}
 }
 
