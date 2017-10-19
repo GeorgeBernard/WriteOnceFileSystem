b15de4a836c6810b4084935c7722138834531fef
NickLockett <NickS.Lockett@gmail.com>
2017-10-19 15:53:15 -0400

    parsing file info built

--- Master.c
+++ Master.c
@@ -6,17 +6,25 @@
 #include <ftw.h>
 #include <iostream>
 #include <stdint.h>
+#include <string>
 
 #include "OnDiskStructure.h"
 
 
-int s_builder(const char *, const struct stat *, int, struct FTW *);
+static int s_builder(const char *, const struct stat *, int, struct FTW *);
 int image();
+static int display_info(const char *, const struct stat *, int, struct FTW *);
+const char* parse_name(const char *);
 
-const int MAX_METADATA = 2000;
+
+
+const int MAX_METADATA = 100;
 
 m_prs* meta;
+
 int metadataPointer = 0;
+int header_count = 0;
+int subitems_count = 0;
 
 int main(int argc, char **argv){
 	// initialize our structure
@@ -37,8 +45,16 @@ int main(int argc, char **argv){
             // function to call on each directory/file
             // max number of directories that can be used
             // flags to specialize usage, we aren't using any right now  
-
     int result =  nftw(argv[1], s_builder, MAX_METADATA, 0);		
+
+    for(int i = 0; i < metadataPointer; i++){
+        std::cout << meta[i].name << "\n";
+        std::cout << meta[i].type << "\n";
+        std::cout << meta[i].length << "\n";
+    }
+    //std::cout << header_count << "\n";
+    //std::cout << subitems_count << "\n";
+
 
     // Now write the file to a structure
     image();
@@ -46,31 +62,18 @@ int main(int argc, char **argv){
 }
 
 // function called on each sub directory/file, updates the global information
-int s_builder(const char * path_name, const struct stat * object_info, int ftw, struct FTW * data){
-    std::cout << "Called s_builder on " << path_name << "\n";
+static int s_builder(const char * path_name, const struct stat * object_info, int ftw, struct FTW * data){
+    //std::cout << "Called s_builder on:" << "\n"; 
+    //std::cout << path_name << "\n";
+    header_count++;
 	// store directory metadata
 	if(ftw == FTW_D){
 		//get filename
-        char* path_name2;
-        strncpy(path_name2, path_name, strlen(path_name)); 
-        char* buf;
-        buf = std::strtok(path_name2, "/");
-        char* buff;
-        while (buf != NULL){   
-            buff = buf;
-            buf = std::strtok (NULL, "/");
-        }
-
-		char* file_name = (char*) malloc(sizeof(char)*256);
-        for(int i =0; i < sizeof(buff); i++){
-            file_name[i] = buff[i];
-        }
-        std::cout << file_name <<"\n";
+        const char* file_name = parse_name(path_name);
 
 		// get number of files in directory algo from: https://stackoverfloxw.com/questions/1723002/how-to-list-all-subdirectories-in-a-given-directory-in-c?answertab=votes#tab-top
-		int dir_length = 0;
+        int dir_length = 0;
     	struct dirent* d;
-    	// TODO Consider this- 2 loops or one loop and then rebuilding loop?
     	DIR* rdir = opendir(path_name);
     	while((d = readdir(rdir)) != NULL)
     	{
@@ -83,7 +86,9 @@ int s_builder(const char * path_name, const struct stat * object_info, int ftw, 
         	}
     	}
     	closedir(rdir);
+
 		// assign all values
+        subitems_count = subitems_count + dir_length;
         for(int i =0; i < 256; i++){
             meta[metadataPointer].name[i] = file_name[i];
         }
@@ -92,33 +97,18 @@ int s_builder(const char * path_name, const struct stat * object_info, int ftw, 
 		meta[metadataPointer].length = dir_length;
 		meta[metadataPointer].time = object_info->st_mtime;
 		meta[metadataPointer].p = (void*) opendir(path_name);
-
 		metadataPointer++;
         return 0;
 	}
 	// Store File Metadata - possibly add in FTW_NS and FTW_SNL functionality for failed symbolic links
 	else if((ftw == FTW_F) || (ftw == FTW_SL)){
 		//get filename
-        char* path_name2;
-        strncpy(path_name2, path_name, strlen(path_name)); 
-        char* buf;
-        buf = std::strtok(path_name2, "/");
-        char* buff;
-        while (buf != NULL){   
-            buff = buf;
-            buf = std::strtok (NULL, "/");
-        }
-
-        char* file_name = (char*) malloc(sizeof(char)*256);
-        for(int i =0; i < sizeof(buff); i++){
-            file_name[i] = buff[i];
-        }
-        std::cout << file_name <<"\n";
-
+        const char* file_name = parse_name(path_name);
 		// assign all values
         for(int i =0; i < 256; i++){
             meta[metadataPointer].name[i] = file_name[i];
         }		
+
         meta[metadataPointer].type = PLAIN_FILE;
 		meta[metadataPointer].length = object_info->st_size;
 		meta[metadataPointer].time = object_info->st_mtime; // time of last modification, could also use atime for last access or ctime for last status change
@@ -126,12 +116,26 @@ int s_builder(const char * path_name, const struct stat * object_info, int ftw, 
 		metadataPointer++;
         return 0;
 	}
+    return 7; // indicates an error
+}
+
+//returns final token separated by /
+const char* parse_name(const char * path_name){
+    std::string s = path_name;
+    std::string d = "/";
+    std::string token;
+    size_t pos = 0;
+    while ((pos = s.find(d)) != std::string::npos) {
+        token = s.substr(0, pos);
+        s.erase(0, pos + d.length());
+    }
+
+    return s.c_str();
 }
 
 // function to write the file 
 int image(){
-	
-
+	return 0;
 }
 
 
