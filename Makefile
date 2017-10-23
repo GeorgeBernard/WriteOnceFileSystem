36a0e2ddcbd125982de54b44436cb0bc23b936ea
Ryan St.Pierre <ras70@duke.edu>
2017-10-23 00:09:10 -0400

    basic working implementation

--- Makefile
+++ Makefile
@@ -1,9 +1,12 @@
 CFLAGS= -Wall -Wextra -std=c++11
 
-all: master.o
+all: master.o old.o
 
 master.o: Master.cpp
 	g++ $(CFLAGS) Master.cpp -o master.out
+
+old.o: old.cpp
+	g++ $(CFLAGS) old.cpp -o old.out
 
 rebuild: clean all
 
--- Master.cpp
+++ Master.cpp
@@ -7,7 +7,6 @@
 #include <iostream>
 #include <stdint.h>
 #include <string>
-#include <algorithm>
 
 #include "OnDiskStructure.h"
 
@@ -29,157 +28,152 @@ int subitems_count = 0;
 
 int main(int argc, char **argv){
 	// initialize our structure
-	meta = new m_prs;
+	meta = (m_prs*) malloc(MAX_METADATA * sizeof(m_prs));
 	// ensure input is appropriate
-	if(argc < 2){
-		std::cout << "Please input a directory to master." << "\n";
-		return 0;
-	}
-	else if(argc > 2){
-		std::cout << "This program currently only supports mastering a single directory."
-		"Package the directories into a single directory and pass that in.  Thanks." << "\n";
-		return 0;
-	}
+    if(argc < 2){
+    	std::cout << "Please input a directory to master." << "\n";
+    	return 0;
+    }
+    else if(argc > 2){
+    	std::cout << "This program currently only supports the mastering of a single directory.  Please package the directories into an additional folder and pass that in.  Thanks." << "\n";
+    	return 0;
+    }
 
-	// Using the nftw() funtion to update global structure
-	// see here: https://www.ibm.com/support/knowledgecenter/en/SSLTBW_2.3.0/com.ibm.zos.v2r3.bpxbd00/rnftw.htm
-	// filepath
-	// function to call on each directory/file
-	// max number of directories that can be used
-	// flags to specialize usage, we aren't using any right now
-	int result =  nftw(argv[1], s_builder, MAX_METADATA, 0);
+    // Using the nftw() funtion to update global structure
+    //	see here: https://www.ibm.com/support/knowledgecenter/en/SSLTBW_2.3.0/com.ibm.zos.v2r3.bpxbd00/rnftw.htm
+            // filepath
+            // function to call on each directory/file
+            // max number of directories that can be used
 
-	// print out metadata properties for testing purposes
-	for(int i = 0; i < metadataPointer; i++){
-		std::cout << meta[i].name << "\n";
-		std::cout << meta[i].type << "\n";
-		std::cout << meta[i].length << "\n";
-	}
-	//std::cout << header_count << "\n";
-	//std::cout << subitems_count << "\n";
+            // flags to specialize usage, we aren't using any right now
+    int result =  nftw(argv[1], s_builder, MAX_METADATA, 0);
+
+    for(int i = 0; i < metadataPointer; i++){
+        std::cout << meta[i].name << "\n";
+        std::cout << meta[i].type << "\n";
+        std::cout << meta[i].length << "\n";
+    }
+    std::cout << header_count << "\n";
+    std::cout << subitems_count << "\n";
 
 
-	// Now write the file to a structure
-	int imageStatus = image();
+    // Now write the file to a structure
+    int imageStatus = image();
 	return 0;
 }
 
 // function called on each sub directory/file, updates the global information
 static int s_builder(const char * path_name, const struct stat * object_info, int ftw, struct FTW * data){
-	std::cout << "Called s_builder on:" << "\n";
-	std::cout << path_name << "\n";
-	std::cout << "ftw: " << ftw << std::endl;
-	header_count++;
-
-	std::cout << "dir: " << FTW_D << std::endl;
-	std::cout << "file: " << FTW_F << std::endl;
-
+		const char* dir_file_name = parse_name(path_name);
+		char firstCharacter = (dir_file_name)[0];
+		if (firstCharacter == '.') {
+			return 0;
+		}
+    header_count++;
 	// store directory metadata
-	if (ftw == FTW_D) { //if directory
+	if(ftw == FTW_D){
 		//get filename
-		const char* file_name = path_name;
-		//const char* file_name = path_name;
-		std::cout << "Dir name: " << file_name << std::endl;
-		// get number of files in directory algo from: https://stackoverflow.com/questions/1723002/how-to-list-all-subdirectories-in-a-given-directory-in-c?answertab=votes#tab-top
-		int dir_length = 0;
+      const char* dir_name = dir_file_name;
+			char* buffer = new char[255];
+ 			std::string str (dir_name);
+ 			std::size_t length = str.copy(buffer,255,0);
+ 			buffer[length]='\0';
 
-		struct dirent* d;
-		DIR* rdir = opendir(path_name);
-		while ((d = readdir(rdir)) != NULL) {
-			struct stat st;
-			if (fstatat(dirfd(rdir), d->d_name, &st, 0) < 0){
-				perror(d->d_name);
-			}
-			dir_length++;
-			// else if(strstr(d->d_name, ".") != NULL && strstr(d->d_name, "..") != NULL){
-			// 	dir_length++;
-			// }
-		}
-		closedir(rdir);
 
+		// get number of files in directory algo from: https://stackoverfloxw.com/questions/1723002/how-to-list-all-subdirectories-in-a-given-directory-in-c?answertab=votes#tab-top
+      int dir_length = 0;
+
+    	struct dirent* d;
+    	DIR* rdir = opendir(path_name);
+    	while((d = readdir(rdir)) != NULL)
+    	{
+        	struct stat st;
+					if (fstatat(dirfd(rdir), d->d_name, &st, 0) < 0){
+            	perror(d->d_name);
+        	}
+        	else{
+						char firstChar = (d-> d_name)[0];
+						if (firstChar == '.') {
+							continue;
+						}
+						std::cout << "d_name: " << d->d_name << std::endl;
+
+        		dir_length++;
+        	}
+    	}
+    	closedir(rdir);
 		// assign all values
-		subitems_count += dir_length;
+        subitems_count = subitems_count + dir_length;
+        for(int i =0; i < 255; i++){
+            meta[metadataPointer].name[i] = buffer[i];
+        }
 
-		for (int i = 0; i < 255; i++) {
-			meta[metadataPointer].name[i] = file_name[i]; // copy over all chars in dir name
-		}
-		meta[metadataPointer].type = DIRECTORY;
-		meta[metadataPointer].length = dir_length;
-		meta[metadataPointer].time = object_info->st_mtime;
-		meta[metadataPointer].p = (void*) opendir(path_name);
-		std::cout << "type: " << meta[metadataPointer].type << std::endl;
-		std::cout << "length: " << meta[metadataPointer].length << std::endl;
-		std::cout << "time: " << meta[metadataPointer].time << std::endl;
-		std::cout << "p: " << meta[metadataPointer].p << std::endl;
-		metadataPointer++;
-		return 0;
+				meta[metadataPointer].type = DIRECTORY;
+				meta[metadataPointer].length = dir_length;
+				meta[metadataPointer].time = object_info->st_mtime;
+				meta[metadataPointer].p = (void*) opendir(path_name);
+				metadataPointer++;
+        return 0;
 	}
-
-	// not a directory
 	// Store File Metadata - possibly add in FTW_NS and FTW_SNL functionality for failed symbolic links
-	else if ((ftw == FTW_F) || (ftw == FTW_SL)) {
+	else if((ftw == FTW_F) || (ftw == FTW_SL)){
 		//get filename
-		const char* file_name = path_name;
+        const char* file_name = dir_file_name;
 		// assign all values
-		for(int i = 0; i < 255; i++){
-			meta[metadataPointer].name[i] = file_name[i];
-		}
-		meta[metadataPointer].type = PLAIN_FILE;
-		meta[metadataPointer].length = object_info->st_size; // need the size
+        for(int i =0; i < 255; i++){
+            meta[metadataPointer].name[i] = file_name[i];
+        }
+
+        meta[metadataPointer].type = PLAIN_FILE;
+		meta[metadataPointer].length = object_info->st_size;
 		meta[metadataPointer].time = object_info->st_mtime; // time of last modification, could also use atime for last access or ctime for last status change
 		meta[metadataPointer].p = fopen(path_name, "r"); // open the file for reading, when writing to img use this stream
-
-		std::cout << "type: " << meta[metadataPointer].type << std::endl;
-		std::cout << "length: " << meta[metadataPointer].length << std::endl;
-		std::cout << "time: " << meta[metadataPointer].time << std::endl;
-		std::cout << "p: " << meta[metadataPointer].p << std::endl;
 		metadataPointer++;
-		return 0;
+        return 0;
 	}
-
-	return 7; // indicates an error
-
+    return 7; // indicates an error
 }
 
-// returns final token separated by /
+//returns final token separated by /
 const char* parse_name(const char * path_name){
-	std::cout << "in parse name: " << path_name << std::endl;
-	std::string s = path_name;
-	std::string d = "/";
-	std::string token;
-	size_t pos = 0;
-	while ((pos = s.find(d)) != std::string::npos) { //until end of string is reached
-		token = s.substr(0, pos);
-		s.erase(0, pos + d.length());
-	}
-	std::cout << "end parse name: " << path_name << std::endl;
-	return s.c_str(); //convert from cpp to c formatted string
-	// char* buffer = new char[s.size()];
-	// std::size_t length = s.copy(buffer, s.size(), 0);
-	// buffer[length]='\0';
-	// std::cout << "end parse name: " << path_name << std::endl;
-	// return buffer;
+    std::string s = path_name;
+    std::string d = "/";
+    std::string token;
+    size_t pos = 0;
+    while ((pos = s.find(d)) != std::string::npos) {
+        token = s.substr(0, pos);
+        s.erase(0, pos + d.length());
+    }
 
+    return s.c_str();
 }
 
 // function to write the file
 int image(){
+	std::cout << "in image" << std::endl;
 	uint64_t header_offset = 0;
 	uint64_t file_offset = find_header_size();
 	uint64_t end_offset = 0;
 
+	std::cout << "file offset: " << file_offset << std::endl;
+
 	FILE *output;
 	output = fopen("./output.wofs", "a"); // open in append mode
 
+
 	//write type dependent information
 	for(int i = 0; i < metadataPointer; i++){
+
+		std::cout << "i: " << i << std::endl;
+		std::cout << "name: " << meta[i].name << std::endl;
 		//Write universal information
 		fwrite(meta[i].name, sizeof(char), 255, output);
 		char lastChar = '\0';
 		fwrite(&lastChar, sizeof(char), 1, output);
-		fwrite((char*) meta[i].length, sizeof(uint64_t), 1, output);
-		fwrite((char*) meta[i].time, sizeof(uint64_t), 1, output);
-		fwrite((char*) meta[i].type, sizeof(uint32_t), 1, output );
+		fwrite((char*) &meta[i].length, sizeof(uint64_t), 1, output);
+		fwrite((char*) &meta[i].time, sizeof(uint64_t), 1, output);
+		fwrite((char*) &meta[i].type, sizeof(uint32_t), 1, output );
+
 
 		if(meta[i].type == PLAIN_FILE){
 			fwrite((char*) &file_offset, sizeof(uint64_t), 1, output);
@@ -200,10 +194,11 @@ int image(){
 		}
 
 		else if (meta[i].type == DIRECTORY){
+			std::cout << "in directory" << std::endl;
 			// Move header offset one header downward
 			header_offset += M_HDR_SIZE;
 			// Write the header offset to point to next block
-			fwrite((char*) header_offset, sizeof(uint64_t), 1, output);
+			fwrite((char*) &header_offset, sizeof(uint64_t), 1, output);
 
 			if (end_offset < header_offset) {
 					end_offset = header_offset;
@@ -253,7 +248,10 @@ int image(){
 	return 0;
 }
 
+
 uint64_t find_header_size(){
-	uint64_t h_size = header_count * sizeof(m_hdr) + subitems_count * sizeof(uint64_t);
-	return h_size;
+		std::cout << "header_count: " << header_count << std::endl;
+		std::cout << "subitems_count: " << subitems_count << std::endl;
+    uint64_t h_size = header_count * M_HDR_SIZE + subitems_count * sizeof(uint64_t);
+		return h_size;
 }
