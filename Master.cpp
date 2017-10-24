68f5d066a7004a038165b2a974f4a72939a65c9b
Ryan St.Pierre <ras70@duke.edu>
2017-10-24 00:04:50 -0400

    moving tree work

--- Master.cpp
+++ Master.cpp
@@ -9,6 +9,8 @@
 #include <stdint.h>
 #include <string>
 #include <endian.h>
+#include <stack>
+#include <queue>
 
 #include "OnDiskStructure.h"
 
@@ -27,10 +29,10 @@ m_prs* meta;
 int metadataPointer = 0;
 int header_count = 0;
 int subitems_count = 0;
+std::stack<node> directories;
 
 int main(int argc, char **argv){
-	// initialize our structure
-	meta = (m_prs*) malloc(MAX_METADATA * sizeof(m_prs));
+
 	// ensure input is appropriate
     if(argc < 2){
     	std::cout << "Please input a directory to master." << "\n";
@@ -41,6 +43,19 @@ int main(int argc, char **argv){
     	return 0;
     }
 
+    //make dummy head node to store ptr to root of the tree
+    m_prs* h = new m_prs;
+    h->type = DIRECTORY;
+    h->length = 1;
+
+    // make dummy head to store first root
+    node* head = new node;
+    head->data = h;
+    head->fill = 0;
+    head->children = new node[1];
+
+    directories.push(*head);
+
     // Using the nftw() funtion to update global structure
     //	see here: https://www.ibm.com/support/knowledgecenter/en/SSLTBW_2.3.0/com.ibm.zos.v2r3.bpxbd00/rnftw.htm
             // filepath
@@ -50,6 +65,30 @@ int main(int argc, char **argv){
             // flags to specialize usage, we aren't using any right now
     int result =  nftw(argv[1], s_builder, MAX_METADATA, 0);
 
+    //iterate to real root
+    node root = head->children[0];
+
+    // initialize our array structure
+    meta = (m_prs*) malloc(metadataPointer * sizeof(m_prs));
+    int i = 0;
+
+    // write the tree to an array  this should be put in a separate method later
+    std::queue<node> q;
+    q.push(root);
+    while(!q.empty()){
+        node t = q.front();
+        q.pop();
+        meta[i] = *t.data;
+        i++;
+        // if not a file recurs through sub files/directories
+        if(t.fill != -1){
+            for(int j = 0; j < t.data->length; j++){
+                q.push(t.children[j]);
+            }
+        }
+    }
+
+    // Write the BFS structure for testing purposes.
     for(int i = 0; i < metadataPointer; i++){
         std::cout << meta[i].name << "\n";
         std::cout << meta[i].type << "\n";
@@ -67,14 +106,16 @@ int main(int argc, char **argv){
 // function called on each sub directory/file, updates the global information
 static int s_builder(const char * path_name, const struct stat * object_info, int ftw, struct FTW * data){
 
-		const char* dir_file_name = parse_name(path_name);
-		char firstCharacter = (dir_file_name)[0];
-		if (firstCharacter == '.') {
-			return 0;
-		}
+	const char* dir_file_name = parse_name(path_name);
+	char firstCharacter = (dir_file_name)[0];
+	if (firstCharacter == '.') {
+		return 0;
+	}
+
     header_count++;
 	// store directory metadata
 	if(ftw == FTW_D){
+
 		//get filename
       const char* dir_name = dir_file_name;
 			char* buffer = new char[255];
@@ -99,38 +140,83 @@ static int s_builder(const char * path_name, const struct stat * object_info, in
 						if (firstChar == '.') {
 							continue;
 						}
-						std::cout << "d_name: " << d->d_name << std::endl;
 
         		dir_length++;
         	}
     	}
     	closedir(rdir);
+
 		// assign all values
+        m_prs* h = new m_prs;
         subitems_count = subitems_count + dir_length;
         for(int i =0; i < 255; i++){
-            meta[metadataPointer].name[i] = buffer[i];
+            h->name[i] = buffer[i];
+        }
+		h->type = DIRECTORY;
+		h->length = dir_length;
+		h->time = object_info->st_mtime;
+		h->p = (void*) opendir(path_name);
+
+        // insert into parent if there is one
+        if(!directories.empty()){
+            node parent = directories.top();
+            directories.pop();
+
+            //write child information
+            parent.children[parent.fill].data = h;
+            parent.children[parent.fill].fill = 0;
+            parent.children[parent.fill].children = new node[h->length];
+
+            //update the "fill" value
+            parent.fill = parent.fill + 1;
+
+            // if not full add it back to the stack
+            if(parent.fill != parent.data->length){
+                directories.push(parent);
+            }
+             // add this directory to the stack
+            directories.push(parent.children[parent.fill-1]);
         }
 
-				meta[metadataPointer].type = DIRECTORY;
-				meta[metadataPointer].length = dir_length;
-				meta[metadataPointer].time = object_info->st_mtime;
-				meta[metadataPointer].p = (void*) opendir(path_name);
-				metadataPointer++;
+
+        //keep total size
+		metadataPointer++;
         return 0;
 	}
 	// Store File Metadata - possibly add in FTW_NS and FTW_SNL functionality for failed symbolic links
 	else if((ftw == FTW_F) || (ftw == FTW_SL)){
 		//get filename
         const char* file_name = dir_file_name;
-		// assign all values
-        for(int i =0; i < 255; i++){
-            meta[metadataPointer].name[i] = file_name[i];
-        }
 
-        meta[metadataPointer].type = PLAIN_FILE;
-		meta[metadataPointer].length = object_info->st_size;
-		meta[metadataPointer].time = object_info->st_mtime; // time of last modification, could also use atime for last access or ctime for last status change
-		meta[metadataPointer].p = fopen(path_name, "r"); // open the file for reading, when writing to img use this stream
+		// assign all values
+        m_prs* h = new m_prs;
+        for(int i =0; i < 255; i++){
+            h->name[i] = file_name[i];
+        }
+        h->type = PLAIN_FILE;
+		h->length = object_info->st_size;
+		h->time = object_info->st_mtime; // time of last modification, could also use atime for last access or ctime for last status change
+		h->p = fopen(path_name, "r"); // open the file for reading, when writing to img use this stream
+
+        // add to the tree
+        if(!directories.empty()){
+            // get parent node
+            node parent = directories.top();
+            directories.pop();
+
+            // add in this node's data
+            parent.children[parent.fill].data = h;
+            parent.children[parent.fill].fill = -1;
+            parent.children[parent.fill].children = NULL;
+
+            //update the parent fill value
+            parent.fill = parent.fill + 1;
+
+            // if the parent is not full add it back
+            if(parent.fill != parent.data->length){
+                directories.push(parent);
+            }
+        }
 		metadataPointer++;
         return 0;
 	}
@@ -140,12 +226,12 @@ static int s_builder(const char * path_name, const struct stat * object_info, in
 void write64(uint64_t item, FILE* output) {
 	uint64_t bigEndian = htobe64(item);
 	fwrite((char*) &bigEndian, sizeof(uint64_t), 1, output);
-} 
+}
 
 void write32(uint32_t item, FILE* output) {
 	uint32_t bigEndian = htobe32(item);
 	fwrite((char*) &bigEndian, sizeof(uint32_t), 1, output);
-} 
+}
 
 //returns final token separated by /
 const char* parse_name(const char * path_name){
@@ -201,7 +287,7 @@ int image(){
 		uint32_t reversedType = htobe32(meta[i].type);
 		fwrite((char*) &reversedLength, sizeof(uint64_t), 1, output);
 		fwrite((char*) &reversedTime, sizeof(uint64_t), 1, output);
-		
+
 
 		if(meta[i].type == PLAIN_FILE){
 			write64(file_offset, output);
