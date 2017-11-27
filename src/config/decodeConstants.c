aacb4d1b7552b68584e08a55950743fe24d0e3cd
George Bernard <ghb5@duke.edu>
2017-11-26 21:10:43 -0500

    add schifra and update decodeconstants

--- src/config/decodeConstants.c
+++ src/config/decodeConstants.c
@@ -1,5 +1,5 @@
 const std::size_t FIELD_DESCRIPTOR    =   8;
 const std::size_t GEN_POLY_INDEX      = 120;
-const std::size_t ROOT_COUNT 		  =   6;
+const std::size_t ROOT_COUNT 		  =  32;
 const std::size_t CODE_LENGTH         = 255;
-const std::size_t FEC_LENGTH          =   6;
+const std::size_t FEC_LENGTH          =  32;
