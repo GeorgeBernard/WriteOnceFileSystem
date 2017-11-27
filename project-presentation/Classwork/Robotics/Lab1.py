da7120022e44bfd05eaa65918f5ecde4cd79467f
ras70lsa <ras70@duke.edu>
2017-11-26 22:30:08 -0500

    adding classwork folder

---
+++ project-presentation/Classwork/Robotics/Lab1.py
@@ -0,0 +1,3 @@
+robot moves right
+robot moves left 
+robot does some more stuff
---
+++ project-presentation/Classwork/Robotics/Lab2.py
@@ -0,0 +1,8 @@
+This lab is a lot harder...
+
+robot moves left
+robot moves right 
+robot moves back
+robot MOVES UP 
+
+DONE
